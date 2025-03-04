b5a767808d76fb244552b5ab13cefb4f32e39dfc
Vaibhav Chitransh <119576891+Vaibhav-Chitransh@users.noreply.github.com>
2025-03-05 00:20:49 +0530

    Update api.ts

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://todo-app-backend-0dir.onrender.com',
+    baseURL: 'https://todo-app-backend-0dir.onrender.com/todos',
 });
 
 export default api;
